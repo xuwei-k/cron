17c9c731593ce5de9178fabdb26c088631d74e74
xuwei-k <6b656e6a69@gmail.com>
2021-12-30 16:32:54 +0900

    add source code

---
+++ cron.js
@@ -0,0 +1,26 @@
+const core = require(`${process.env.GITHUB_WORKSPACE}/node_modules/@actions/core`);
+
+module.exports = ({github, context}) => {
+  (async () => {
+    // https://octokit.github.io/rest.js/v18#search-issues-and-pull-requests
+    const pulls = await github.rest.search.issuesAndPullRequests({
+      q: "is:open+is:pr+author:xuwei-k+archived:false",
+      per_page: 100
+    });
+    const conflicts = [];
+    for (const pull of pulls.data.items) {
+      console.log(pull.url);
+      const pull_req = await github.request(`GET ${pull.pull_request.url}`)
+      if (pull_req.data.mergeable === false) {
+        console.log(pull_req.data.mergeable);
+        conflicts.push(pull.html_url);
+      }
+    }
+    console.log(conflicts);
+    if (conflicts.length == 0) {
+      core.setOutput('value', 'There is no conflict pull requests😀');
+    } else {
+      core.setOutput('value', 'conflict pull requests\\n' + conflicts.join("\\n"));
+    }
+  })();
+};
