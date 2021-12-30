0b95fcc782251ad23a5b1f99c35996f71868015a
kenji yoshida <6b656e6a69@gmail.com>
2021-12-30 15:29:19 +0900

    Create cron.js

---
+++ cron.js
@@ -0,0 +1,21 @@
+module.exports = ({github, context}) => {
+  (async () => {
+    // https://octokit.github.io/rest.js/v18#search-issues-and-pull-requests
+    const pulls = await github.rest.search.issuesAndPullRequests({
+      q: "is:open+is:pr+author:xuwei-k+archived:false",
+      per_page: 100
+    });
+    console.log(pulls);
+    for (const pull of pulls.data) {
+      console.log(pull);
+      const pull_req = await github.rest.pulls.get({
+        owner: pull.repo.owner,
+        repo: pull.repo.repo,
+        pull_number: pull.number
+      });
+      if (pull_req.data.mergeable === false) {
+        // TODO
+      }
+    }
+  })();
+});
